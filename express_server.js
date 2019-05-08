cdba11dbbf859394ed7c3430805da8799d6271c3
Christian <c_pfeiffer325@yahoo.ca>
2019-05-08 05:37:03 +0000

    created a new file that implements a form for entering new urls

--- express_server.js
+++ express_server.js
@@ -2,12 +2,22 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+function generateRandomString() {
+
+}
+
+generateRandomString();
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// function to parse the body of the incoming url
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -32,10 +42,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// page for adding new urls
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 // single url page
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
